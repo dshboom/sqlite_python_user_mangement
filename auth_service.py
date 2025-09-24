4f169bd14bc39ad687c95b82f2a1667e2b82e7a0
dshboom <dshboom@outlook.com>
2025-09-24 12:24:54 +0800

    完善AuthService层

--- auth_service.py
+++ auth_service.py
@@ -5,17 +5,35 @@ class AuthService:
     def __init__(self, user_repository):
         self.user_repository = user_repository
     
-    def _password_hash(self, user_password):
+    def _hash_password(self, user_password):
         salt = bcrypt.gensalt(rounds=12)
         hashed_password = bcrypt.hashpw(user_password.encode('utf-8'), salt)
         return hashed_password.decode('utf-8')
     
-    def _verify_password(self, user_attmpt_password, sql_save_hashed_password):
-        return bcrypt.checkpw(user_attmpt_password.encode('utf-8'), sql_save_hashed_password.encode('utf-8'))
+    def _verify_password(self, user_attempt_password, stored_hashed_password):
+        return bcrypt.checkpw(user_attempt_password.encode('utf-8'), stored_hashed_password.encode('utf-8'))
     
-    def register():
-        pass
-    
-    def login():
-        pass
-    
+    def register(self, user_name, user_password):
+        if self.user_repository.find_user_by_username(user_name):
+            return False, "This name already exist!"
+        else:
+            hashed_password = self._hash_password(user_password)
+            if self.user_repository.register_user(user_name, hashed_password):
+                print("认证服务层：注册成功！")
+                return True, "Register successfully!"
+            else:
+                print("认证服务层：注册失败！")
+                return False, "Register failed!"
+            
+    def login(self, user_name, user_attempt_password):
+        sql_save_user_password = self.user_repository.find_user_by_username(user_name)
+        if sql_save_user_password:
+            if self._verify_password(user_attempt_password,sql_save_user_password):
+                print("认证服务层：登录成功！")
+                return True, "Login successfully"
+            else:
+                print("认证服务层：用户名或密码错误！")
+                return False, "User's name or password error!"
+        else:
+            print("认证服务层：用户名或密码错误！")
+            return False, "User's name or password error!"
